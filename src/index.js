2177776e499c4aeb4131db24f58b3c1bca77c856
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-01-04 19:37:35 -0500

    Badges List

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 import './global.css';
 //import Badge from './components/Badge';
-import BadgeNew from './pages/BadgeNew';
+import Badges from './pages/Badges';
 
 const container = document.getElementById('app');
 
-ReactDOM.render(<BadgeNew />
+ReactDOM.render(<Badges />
 ,
   container
 );
