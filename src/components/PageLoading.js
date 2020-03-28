976503146b773fc448b160fd92d79128556c9aad
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-03-28 16:59:19 -0500

    new loader, fetching from remote api

---
+++ src/components/PageLoading.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import "./styles/PageLoading.css";
+import Loader from "./Loader";
+
+export default () => {
+  return (
+    <div className="PageLoading">
+      <Loader />
+    </div>
+  );
+};
