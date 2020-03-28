9db7a6d8bf0c28ebc121cbc679e19486fe36ae9c
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-03-28 15:59:50 -0500

    Adding static records locally

--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,24 @@
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
 
-import BadgeNew from '../pages/BadgeNew';
-import Badges from '../pages/Badges';
-import NotFound from '../pages/NotFound';
+import BadgeNew from "../pages/BadgeNew";
+import Badges from "../pages/Badges";
+import NotFound from "../pages/NotFound";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Badges} />
-                    <Route exact path="/badges/new" component={BadgeNew} />
-                    <Route component={NotFound} />
-                </Switch>
-            </Layout>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/" component={Badges} />
+          <Route exact path="/badges/new" component={BadgeNew} />
+          <Route component={NotFound} />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
