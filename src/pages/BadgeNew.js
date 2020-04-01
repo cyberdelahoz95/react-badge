4b5eab1994feab8d5b04e2ec864d8a017cba09d9
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-03-31 21:00:05 -0500

    Improving UI Badge New

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,58 +1,68 @@
-import React from 'react';
+import React from "react";
 
-import './styles/BadgeNew.css';
+import "./styles/BadgeNew.css";
 
-import Badge from '../components/Badge';
-import BadgeForm from '../components/BadgeForm';
+import Badge from "../components/Badge";
+import BadgeForm from "../components/BadgeForm";
 
-import header from '../images/badge-header.svg';
+import header from "../images/platziconf-logo.svg";
 
 class BadgeNew extends React.Component {
-    state={
-        form:{
-            firstName:'',
-            lastName:'',
-            email:'',
-            jobTitle:'',
-            twitter:'',
+    state = {
+        form: {
+            firstName: "",
+            lastName: "",
+            email: "",
+            jobTitle: "",
+            twitter: ""
         }
     };
-    handleChange = e =>{        
+    handleChange = e => {
         this.setState({
             form: {
                 ...this.state.form,
-                [e.target.name]: e.target.value,
+                [e.target.name]: e.target.value
             }
-        })
-    }
+        });
+    };
     render() {
         return (
-            <React.Fragment>                
+            <React.Fragment>
                 <div className="BadgeNew__hero">
-                    <img className="img-fluid" src={header} alt="Logo" />
+                    <img
+                        className="BadgeNew__hero-image img-fluid"
+                        src={header}
+                        alt="Logo"
+                    />
                 </div>
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
-                            <Badge 
-                                firstName={this.state.form.firstName} 
-                                lastName={this.state.form.lastName}
-                                twitter={this.state.form.twitter}
-                                jobTitle={this.state.form.jobTitle}
-                                email={this.state.form.email}
+                            <Badge
+                                firstName={
+                                    this.state.form.firstName || "First Name"
+                                }
+                                lastName={
+                                    this.state.form.lastName || "last Name"
+                                }
+                                twitter={this.state.form.twitter || "Twitter"}
+                                jobTitle={
+                                    this.state.form.jobTitle || "Job Title"
+                                }
+                                email={this.state.form.email || "Email"}
                                 avatarUrl="https://pbs.twimg.com/profile_images/1073709379646615552/TJBGcGV5_400x400.jpg"
                             />
-                        </div>       
+                        </div>
                         <div className="col-6">
-                            <BadgeForm 
-                                onChange={this.handleChange}      
-                                formValues={this.state.form}                     
-                            /> 
-                        </div>               
+                            <BadgeForm
+                                onChange={this.handleChange}
+                                formValues={this.state.form}
+                            />
+                        </div>
                     </div>
-                </div>            
+                </div>
             </React.Fragment>
-        )
+        );
     }
 }
 
